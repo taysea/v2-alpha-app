22554ab341cf7ab863a5fe2cfb6650cd7b93dcf0
Matthew Glissmann <mdglissmann@gmail.com>
2025-05-21 17:03:55 -0600

    Added router

---
+++ src/pages/Examples.tsx
@@ -0,0 +1,24 @@
+import { Anchor, Data, List, Page, PageContent, PageHeader, ThemeContext } from "grommet";
+import { hpe } from "grommet-theme-hpe";
+
+const examples = [
+    {label: 'Sticker sheet', href: '/sticker-sheet'},
+    {label: 'Insider', href: '/insider'}
+];
+
+export const Examples = () => {
+    return (
+        <ThemeContext.Extend value={hpe}>
+        <Page background="background-back" height="100vh">
+            <PageContent>
+                <PageHeader title="Examples of V2-alpha theme" />
+                <Data data={examples} >
+                    <List>
+                        {(item: {label: string, href:string}) => <Anchor label={item.label} href={item.href} />}
+                    </List>
+                </Data>
+            </PageContent>
+        </Page>
+        </ThemeContext.Extend>
+    );
+}
